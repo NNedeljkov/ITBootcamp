38d50748f999af69a33e251d9ff96e0071632f96
Nemanja Nedeljkov <srafcigerintj@gmail.com>
2020-07-26 17:43:03 +0200

    Domaci zadatak za Nedelju, nedelja 2 (mala prepravka)

--- Nedelja/zadatak1.js
+++ Nedelja/zadatak1.js
@@ -1,8 +1,10 @@
 // povrsina pice
-let R = 40
-let cena = 1150
-let r = R / 2
-let P = r * r * Math.PI
-let cenaPoCmKv = cena / P
-console.log(`Cena pice po centimetru kvadratnom iznosi ${cenaPoCmKv} dinara.`)
-console.log(`Ukupna cena pice iznosi ${cenaPoCmKv * P} dinara.`) //trebalo bi da ispise cenu pice (vrednost varijable "cena"), cisto zbog provere
+{
+    let R = 40
+    let cena = 1150
+    let r = R / 2
+    let P = r * r * Math.PI
+    let cenaPoCmKv = cena / P
+    console.log(`Cena pice po centimetru kvadratnom iznosi ${cenaPoCmKv} dinara.`)
+    console.log(`Ukupna cena pice iznosi ${cenaPoCmKv * P} dinara.`) //trebalo bi da ispise cenu pice (vrednost varijable "cena"), cisto zbog provere}
+}
